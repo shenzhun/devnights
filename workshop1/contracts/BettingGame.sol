5fbc03c48f05cf5697948dc23bf32d0295d9eed7
shenzhun <shenzhunallen@gmail.com>
2019-03-16 12:50:57 -0700

    truffle project

--- workshop1/contracts/BettingGame.sol
+++ workshop1/contracts/BettingGame.sol
@@ -1,23 +1,27 @@
 pragma solidity ^0.4.21;
 
+import './SafeMath.sol';
+
 contract BettingGame {
     
     address public owner;
     
     struct Bet {
-        safeuint choice;
-        safeuint amount;
+        uint256 choice;
+        uint256 amount;
         bool paid;
         bool initialized;
     }
     mapping(address => Bet) bets;
-    
-    safeuint total_bet_amount;
-    mapping(safeuint => safeuint) choice_bet_amounts;
-    
-    safeuint correct_choice;
 
-    safeuint game_status; // 0 not started; 1 running; 2 ended
+    using SafeMath for uint256;
+    
+    uint256 total_bet_amount;
+    mapping(uint256 => uint256) choice_bet_amounts;
+    
+    uint256 correct_choice;
+
+    uint256 game_status; // 0 not started; 1 running; 2 ended
     
     modifier onlyOwner() {
         assert(msg.sender == owner);
@@ -34,7 +38,7 @@ contract BettingGame {
         game_status = 1;
     }
     
-    function placeBet (safeuint _choice) public payable {
+    function placeBet (uint256 _choice) public payable {
         require (game_status == 1); // game is running
         require (_choice > 0);
         require (_choice <= 12);
@@ -48,7 +52,7 @@ contract BettingGame {
         total_bet_amount = total_bet_amount + msg.value;
     }
     
-    function endGame(safeuint _correct_choice) external onlyOwner {
+    function endGame(uint256 _correct_choice) external onlyOwner {
         correct_choice = _correct_choice;
         game_status = 2;
     }
@@ -60,14 +64,14 @@ contract BettingGame {
         require (bets[msg.sender].choice == correct_choice); // chose correctly
         require (bets[msg.sender].paid == false); // chose correctly
         
-        safeuint payout = bets[msg.sender].amount * total_bet_amount / choice_bet_amounts[correct_choice];
+        uint256 payout = bets[msg.sender].amount * total_bet_amount / choice_bet_amounts[correct_choice];
         if (payout > 0) {
             msg.sender.transfer(uint256(payout));
             bets[msg.sender].paid == true; // cannot claim twice
         }
     }
 
-    function getAnswer() public view returns (safeuint) {
+    function getAnswer() public view returns (uint256) {
         return (correct_choice);
     }
   
